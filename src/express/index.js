608ba663758b8d619898a5fbcfb8e08f8c49a63f
Hopman <3tonchik@mail.ru>
2020-03-29 21:14:16 +0300

    module3-task2

--- src/express/index.js
+++ src/express/index.js
@@ -8,11 +8,18 @@ const express = require(`express`);
 const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
+const path = require(`path`);
+const PUBLIC_DIR = `public`;
+
+
 
 // Зафиксируем порт для сервера
 const DEFAULT_PORT = 8080;
 
 const app = express();
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 // Подключим созданные маршруты
 app.use(`/offers`, offersRoutes);
