50777ed38c5455dbc57781e4c912f84af2baaabe
Hopman <3tonchik@mail.ru>
2020-04-13 20:29:12 +0300

    module3-task3

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -8,28 +8,29 @@ const {HttpCode} = require(`../../constants`);
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
 
-const app = express();
-app.use(express.json());
-
-app.get(`/offers`, async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(FILENAME);
-    const mocks = JSON.parse(fileContent);
-    res.json(mocks);
-  } catch (err) {
-    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
-  }
-});
-
-app.use((req, res) => res
-  .status(HttpCode.NOT_FOUND)
-  .send(`Not found`));
-
 module.exports = {
   name: `--server`,
   run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+    const app = express();
+
+    app.use(express.json());
+
+    app.get(`/offers`, async (req, res) => {
+      try {
+        const fileContent = await fs.readFile(FILENAME);
+        const mocks = JSON.parse(fileContent);
+        res.json(mocks);
+      } catch (err) {
+        console.error(`Ошибка роута /offers :`, err);
+        res.status(HttpCode.INTERNAL_SERVER_ERROR);
+      }
+    });
+
+    app.use((req, res) => res
+      .status(HttpCode.NOT_FOUND)
+      .send(`Not found`));
 
     app.listen(port, (err) => {
       if (err) {
